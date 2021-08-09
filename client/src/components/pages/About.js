fcb2fab13bfbde52e8dde7be404041a2686f8dd0
Sudais <44402465+sudaisx@users.noreply.github.com>
2021-08-09 21:23:19 +0500

    commit

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About This App</h1>
+            <p className='my-1'>This is a Full Stack React App for keeping Contacts</p>
+            <p className='p'>
+                <strong>Version: </strong>1.0.0
+            </p>
+        </div>
+    );
+};
+
+export default About;
